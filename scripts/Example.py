44ec3c1e2bb6d9abee104f874bf6d9174050ab39
altingjonbalaj20 <gjonbalaj2017@gmail.com>
2022-03-16 00:45:37 +0100

    made group by date and calculated sum

--- scripts/Example.py
+++ scripts/Example.py
@@ -6,9 +6,15 @@ dataset = pd.read_excel('Files\InvoiceListReport_TP24_20220307.xlsx')
 # print(dataset.keys())
 dataset = dataset[['Invoice Amount', 'Eligible Amount', 'Invoice Date']]
 
-today = datetime.datetime.today().date()
-lastweek = today - datetime.timedelta(days=7)
-print(lastweek)
-
+today = datetime.datetime.today()
+lastweek = today - datetime.timedelta(days=30)
 dataset['Invoice Date'] = pd.to_datetime(dataset['Invoice Date'])
+dataset = dataset[(dataset['Invoice Date'] >= lastweek)]
+
 dataset['Day of Week'] = dataset['Invoice Date'].dt.day_name()
+
+dataset = dataset.groupby(['Day of Week']).sum()
+days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday','Thursday', 'Friday', 'Saturday']
+
+## order index by ordercriteria on days
+

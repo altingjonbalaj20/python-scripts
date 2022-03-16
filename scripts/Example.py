24cfba32acd0d4a2f4aa4649a1f859094e7489aa
altingjonbalaj20 <gjonbalaj2017@gmail.com>
2022-03-16 14:41:05 +0100

    finished line and column plot

--- scripts/Example.py
+++ scripts/Example.py
@@ -7,7 +7,7 @@ dataset = pd.read_excel('Files\InvoiceListReport_TP24_20220307.xlsx')
 dataset = dataset[['Invoice Amount', 'Eligible Amount', 'Invoice Date']]
 
 today = datetime.datetime.today()
-lastweek = today - datetime.timedelta(days=30)
+lastweek = today - datetime.timedelta(days=24)
 dataset['Invoice Date'] = pd.to_datetime(dataset['Invoice Date'])
 dataset = dataset[(dataset['Invoice Date'] >= lastweek)]
 
@@ -15,9 +15,17 @@ dataset['Day of Week'] = dataset['Invoice Date'].dt.day_name()
 
 dataset = dataset.groupby(['Day of Week']).sum()
 days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday','Thursday', 'Friday', 'Saturday']
+#Sortimi ne baze te diteve
+dataset.index = pd.CategoricalIndex(dataset.index, categories=days, ordered=True)
+dataset = dataset.sort_index()
 
-dataset.index = pd.CategoricalDtype(days, ordered=True, dtype='category')
+fig, ax = plt.subplots()
+dataset['Invoice Amount'].plot(kind='bar', color='magenta')
+dataset['Eligible Amount'].plot(kind='line', marker='.', color='black', ms=10)
 
-print(dataset)
+current_values = plt.gca().get_yticks()
+plt.gca().set_yticklabels(['{:,.0f}'.format(x) for x in current_values])
+
+plt.show()
 ## order index by ordercriteria on days
 

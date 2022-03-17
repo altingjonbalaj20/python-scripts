0a46389c7ccb856e655138674a110fcd1f8e838d
altingjonbalaj20 <gjonbalaj2017@gmail.com>
2022-03-17 17:20:17 +0100

    final DailyAmount

--- scripts/DailyAmountByCountry.py
+++ scripts/DailyAmountByCountry.py
@@ -4,6 +4,7 @@ import datetime
 import math
 import numpy as np
 import seaborn as sns
+import copy
 
 dataset = pd.read_excel('Files\InvoiceListReport_TP24_20220307.xlsx')
 dataset = dataset[['Invoice Amount','Invoice Date', 'Country']]
@@ -11,16 +12,17 @@ dataset = dataset[['Invoice Amount','Invoice Date', 'Country']]
 countries = ['Switzerland', 'Italy', 'Germany', 'France', 'Belgium', 'Denmark']
 dataset = dataset.groupby(['Country'])
 days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday','Thursday', 'Friday', 'Saturday']
-orderCriteria = pd.CategoricalIndex(days, days, ordered=True)
-print(orderCriteria)
+
 for i in range(0, len(countries)):
     temp = dataset.get_group(countries[i])
     temp['Invoice Date'] = pd.to_datetime(temp['Invoice Date'])
     temp[countries[i]] = temp['Invoice Date'].dt.day_name()
     temp = temp.groupby(countries[i]).sum()
+    # renditja ne baze te diteve me radhe, dhe emertimi i index per paraqitje tek vizualizimi
+    orderCriteria = pd.CategoricalIndex(days, days, ordered=True, name=countries[i])
     temp = temp.reindex(orderCriteria, fill_value = 0)
+
     countries[i] = temp
-print(countries[0])
 columns = 2;
 rows = math.ceil(len(countries) / columns)
 fig, ax = plt.subplots(nrows = rows, ncols = columns)
